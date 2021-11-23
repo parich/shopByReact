970e7f082a6cd429cb9e5fc10a9bb184569f8f72
parich <parich.suriya@gamil.com>
2021-11-23 15:21:33 +0700

    add cartReducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,17 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { cartReducers } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
 
-const initialStare = {};
+const initialStare = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
+    }
+};
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducers
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
