24abeefe815ea67a71993e3734425f3df76c45b0
parich <parich.suriya@gamil.com>
2021-12-13 17:14:53 +0700

    updateProfile

---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,22 @@
+import { Redirect, Route } from 'react-router-dom'
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export default function PrivateRoute({ component: Component, ...rest }) {
+    const userSignin = useSelector(state => state.userSignin);
+
+    const { userInfo } = userSignin;
+
+    return (
+        <Route
+            {...rest}
+            render={(props) =>
+                userInfo ? (
+                    <Component {...props}></Component>
+                ) : (
+                    <Redirect to='/signin' />
+                )
+            }
+        ></Route>
+    );
+}
